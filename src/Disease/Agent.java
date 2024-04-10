73b1829061d785070dd8dd7df724b13743b1d208
Pallav Regmi <pregmi@unm.edu>
2024-04-10 16:46:51 -0600

    Skeleton pushed

---
+++ src/Disease/Agent.java
@@ -0,0 +1,9 @@
+package Disease;
+public class Agent {
+    public enum State { VULNERABLE, SICK, IMMUNE, DEAD, GHOST }
+
+    private State state;
+    private double x, y;
+
+
+}
---
+++ src/Disease/SimulationEngine.java
@@ -0,0 +1,12 @@
+package Disease;
+
+import java.util.List;
+
+public class SimulationEngine {
+    private List<Agent> agents;
+    private double exposureDistance;
+    private int incubationPeriod;
+    private double recoveryProbability;
+
+
+}
